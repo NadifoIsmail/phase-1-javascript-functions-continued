a52b12f7749058fe80d0496260c1168fd9bb8280
Nadifo Ismail <nadifoismail80@gmail.com>
2024-10-01 12:26:49 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+ return `This Saturday, I want to ${activity}!`
+}
+console.log (saturdayFun());
+console.log (saturdayFun('swim'));
+
+function mondayWork(work = 'go to the office'){
+    return `This Monday, I will ${work}.`
+}
+console.log(mondayWork());
+console.log(mondayWork('work from home'));
+
+function wrapAdjective(par1 = '*') {
+    return function (par2 = 'special') {
+        return `You are ${par1}${par2}${par1}!`
+    } 
+}
+
+let results1 = wrapAdjective()();
+let results2 = wrapAdjective('||')('a dedicated programmer');
+
+console.log(results1);
+console.log(results2);
